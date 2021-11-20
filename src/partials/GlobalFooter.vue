580a14ac34e759a24bf58203d38150be005f3f5b
Taisuke Fukuno <fukuno@jig.jp>
2021-11-21 04:10:36 +0900

    add footer

--- src/partials/GlobalFooter.vue
+++ src/partials/GlobalFooter.vue
@@ -2,6 +2,7 @@
   <footer class="GlobalFooter">
     <div class="GlobalFooter__admin">
       <div>{{ $developper[$i18n.locale] }}</div>
+      <div><a href=https://github.com/code4fukui/tokyobichikunavi>備蓄ナビ src on GitHub</a>, forked from <a href=https://www.bichiku.metro.tokyo.lg.jp/download/>東京備蓄ナビ オープンソース</a></div>
     </div>
   </footer>
 </template>
